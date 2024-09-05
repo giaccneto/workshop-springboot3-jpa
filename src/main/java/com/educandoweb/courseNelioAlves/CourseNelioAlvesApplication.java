74c209c0d903a7bae111083987b70ba4f745d81f
Giacomo <giaccneto@hotmail.com>
2024-09-04 20:48:18 -0400

    Project created

---
+++ src/main/java/com/educandoweb/courseNelioAlves/CourseNelioAlvesApplication.java
@@ -0,0 +1,13 @@
+package com.educandoweb.courseNelioAlves;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CourseNelioAlvesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CourseNelioAlvesApplication.class, args);
+	}
+
+}
