da8dfc10f2aa680b6876cd8cfe38ff990e75e61f
Giacomo <giaccneto@hotmail.com>
2024-09-06 06:49:00 -0400

    OrderStatusEnum

--- src/main/java/com/educandoweb/courseNelioAlves/entities/Order.java
+++ src/main/java/com/educandoweb/courseNelioAlves/entities/Order.java
@@ -1,5 +1,6 @@
 package com.educandoweb.courseNelioAlves.entities;
 
+import com.educandoweb.courseNelioAlves.enums.OrderStatus;
 import com.fasterxml.jackson.annotation.JsonFormat;
 import jakarta.persistence.*;
 
@@ -21,6 +22,8 @@ public class Order implements Serializable {
     @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss'Z'", timezone = "GMT")
     private Instant moment;
 
+    private Integer orderStatus;
+
     @ManyToOne
     @JoinColumn(name = "client_id")
     private User client;
@@ -29,10 +32,12 @@ public class Order implements Serializable {
 
     }
 
-    public Order(Long id, Instant moment, User client) {
+    public Order(Long id, Instant moment,OrderStatus orderStatus, User client) {
         this.id = id;
         this.moment = moment;
+        setOrderStatus(orderStatus);
         this.client = client;
+
     }
 
     public Long getId() {
@@ -49,6 +54,16 @@ public class Order implements Serializable {
 
     public void setMoment(Instant moment) {
         this.moment = moment;
+    }
+
+    public OrderStatus getOrderStatus() {
+        return OrderStatus.valueOf(orderStatus);
+    }
+
+    public void setOrderStatus(OrderStatus orderStatus) {
+        if (orderStatus != null) {
+            this.orderStatus = orderStatus.getCode();
+        }
     }
 
     public User getClient() {
